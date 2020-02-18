c2e9c43895ab7d2768c3ac192cc01f5182260cf2
Unknown <allium.fistulosum.39@gmail.com>
2020-02-19 07:21:19 +0900

    hoge

---
+++ WANNRelease/WANN/tree.py
@@ -0,0 +1,18 @@
+from graphviz import Digraph
+
+# G = Digraph(format="png")
+# G.attr("node", shape="square", style="filled")
+# G.node("start", shape="circle", color="pink")
+# G.edge("start","state1",label="0.8")
+# G.edge("start","state2",label="0.2")
+# G.edge("state1","state1",label="0.5")
+# G.edge("state2","state2", label="0.8")
+# G.edge("state1","state2",label="0.5")
+# G.edge("state2","end",label="0.2")
+# G.edge("end","count",label="1.0")
+# G.edge("count","start",label="1.0")
+# # print()するとdot形式で出力される
+# print(G)
+
+G = Digraph(format="png",filename="graphs")
+G.render("graphs")
